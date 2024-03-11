957af2ab55abfe1444d83a2dc80a888010fc664c
Mileto Di Marco <mileto.di.marco@howest.be>
2024-03-11 18:38:07 +0100

    Add productsController

---
+++ Pri.WebApi.Food.Api/Controllers/ProductsController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Pri.CleanArchitecture.Core.Interfaces.Services;
+
+namespace Pri.WebApi.Food.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly IProductService _productService;
+
+        public ProductsController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            return Ok(await _productService.GetAllAsync());
+        }
+        [HttpGet("{id:int}")]
+        public IActionResult Get(int id)
+        {
+            return Ok($"Products with id:{id}");
+        }
+
+    }
+}
